54c1d4b8a25302e83ce8966c08138b08e34483f3
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2016-09-02 14:18:34 +0200

    Add LayerUpdateState object to manager updates cycle
    Maintaining a state (success, pending, error) to make better informed
    decision: should I update this layer? did it failed earlier? etc

--- src/Scene/LayerUpdateStrategy.js
+++ src/Scene/LayerUpdateStrategy.js
@@ -53,3 +53,55 @@ export function chooseNextLevelToFetch(strategy, nodeLevel, currentLevel, option
             return _minimizeNetworkTraffic(nodeLevel, currentLevel, options);
     }
 }
+
+
+const UPDATE_IDLE = 0;
+const UPDATE_PENDING = 1;
+const UPDATE_ERROR = 2;
+
+const PAUSE_BETWEEN_ERRORS = [ 1000.0, 3000.0, 7000.0 ];
+
+function LayerUpdateState() {
+    this.state = UPDATE_IDLE;
+    this.lastErrorTimestamp = 0;
+    this.errorCount = 0;
+
+}
+
+LayerUpdateState.prototype.constructor = LayerUpdateState;
+
+LayerUpdateState.prototype.canTryUpdate = function(timestamp) {
+    switch (this.state) {
+        case UPDATE_IDLE: {
+            return true;
+        }
+        case UPDATE_PENDING: {
+            return false;
+        }
+        case UPDATE_ERROR:
+        default: {
+            if (3 <= this.errorCount) {
+                return false;
+            } else {
+                return PAUSE_BETWEEN_ERRORS[this.errorCount] <= (timestamp - this.lastErrorTimestamp);
+            }
+        }
+    }
+}
+
+LayerUpdateState.prototype.try = function() {
+    this.state = UPDATE_PENDING;
+}
+
+LayerUpdateState.prototype.success = function() {
+    this.lastErrorTimestamp = 0;
+    this.state = UPDATE_IDLE;
+}
+
+LayerUpdateState.prototype.failure = function(timestamp) {
+    this.lastErrorTimestamp = timestamp;
+    this.state = UPDATE_ERROR;
+    this.errorCount++;
+}
+
+export { LayerUpdateState };
