19d2d756d56c2b0ebdbe299cded6b3d477d5efd8
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2016-09-02 14:12:59 +0200

    Throw custom error when cancelling commands
    So the caller can act differently if needed.

--- src/Core/Commander/ManagerCommands.js
+++ src/Core/Commander/ManagerCommands.js
@@ -178,7 +178,13 @@ ManagerCommands.prototype.getProviders = function() {
     return p;
 };
 
+function CancelledCommandException(command) {
+    this.command = command;
+}
 
+CancelledCommandException.prototype.toString = function() {
+    return 'Cancelled command ' + this.command + ' for node ' + this.command.requester;
+}
 
 /**
  */
@@ -189,7 +195,7 @@ ManagerCommands.prototype.deQueue = function(queue) {
 
         if (cmd.earlyDropFunction && cmd.earlyDropFunction(cmd)) {
             queue.counters.cancelled++;
-            cmd.reject(new Error('command canceled ' + cmd.requester.id + '/' + cmd.layer.id));
+            cmd.reject(new CancelledCommandException(cmd));
         } else {
             return cmd;
         }
@@ -205,6 +211,7 @@ ManagerCommands.prototype.wait = function() {
     this.eventsManager.wait();
 };
 
+export { CancelledCommandException };
 
 export default function(scene) {
     instanceCommandManager = instanceCommandManager || new ManagerCommands(scene);
