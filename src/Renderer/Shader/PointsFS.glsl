dcf4c9c44c3d0045b8a098096eb51871dd35cc5b
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2018-06-25 17:51:53 +0200

    feat(core): add a Points class
    Adding a class allows to implement more easily itowns specific features.
    It also allows to unify the API of itowns drawable / updatable objects.

--- src/Renderer/Shader/PointsFS.glsl
+++ src/Renderer/Shader/PointsFS.glsl
@@ -4,14 +4,30 @@ precision highp int;
 #include <logdepthbuf_pars_fragment>
 
 varying vec4 vColor;
+uniform bool pickingMode;
+uniform float opacity;
+
+uniform bool useCustomColor;
+uniform vec3 customColor;
 
 void main() {
     // circular point rendering
-    if(length(gl_PointCoord - 0.5) > 0.5){
+    float u = 2.0 * gl_PointCoord.x - 1.0;
+    float v = 2.0 * gl_PointCoord.y - 1.0;
+    float cc = u*u + v*v;
+    if(cc > 1.0){
         discard;
     }
 
-    gl_FragColor = vColor;
+    if (useCustomColor && !pickingMode) {
+        gl_FragColor = mix(vColor, vec4(customColor, 1.0), 0.5);
+    } else {
+        gl_FragColor = vColor;
+    }
+
+    if (!pickingMode) {
+        // gl_FragColor.a = opacity;
+    }
 
     #include <logdepthbuf_fragment>
 }
--- src/Renderer/Shader/PointsVS.glsl
+++ src/Renderer/Shader/PointsVS.glsl
@@ -4,16 +4,21 @@ precision highp int;
 #include <logdepthbuf_pars_vertex>
 #define EPSILON 1e-6
 
-attribute vec3 position;
+uniform float size;
+uniform float scale;
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
-uniform float size;
-
+uniform mat4 modelMatrix;
+uniform vec2 resolution;
 uniform bool pickingMode;
-uniform float opacity;
-uniform vec4 overlayColor;
-attribute vec3 color;
+uniform sampler2D texture;
+uniform vec4 offsetScale;
+uniform vec2 extentTopLeft;
+uniform vec2 extentSize;
+
 attribute vec4 unique_id;
+attribute vec3 color;
+attribute vec3 position;
 
 varying vec4 vColor;
 
@@ -21,15 +26,32 @@ void main() {
     if (pickingMode) {
         vColor = unique_id;
     } else {
-        vColor = vec4(mix(color, overlayColor.rgb, overlayColor.a), opacity);
+        vColor = vec4(color, 1.0);
     }
 
-    gl_Position = projectionMatrix * (modelViewMatrix * vec4( position, 1.0 ));
+    vec2 pp = (modelMatrix * vec4(position, 1.0)).xy;
+    // offsetScale is from topleft
+    pp.x -= extentTopLeft.x;
+    pp.y = extentTopLeft.y - pp.y;
+    pp *= offsetScale.zw / extentSize;
+    pp += offsetScale.xy;
+    pp.y = 1.0 - pp.y;
+    vColor = texture2D(texture, pp);
+    // vColor.a = 1.0;
 
-    if (size > 0.) {
+    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+    mat4 projModelViewMatrix = projectionMatrix * modelViewMatrix;
+    gl_Position = projModelViewMatrix* vec4( position, 1.0);
+
+    if (size > 0.01) {
         gl_PointSize = size;
     } else {
-        gl_PointSize = clamp(-size / gl_Position.w, 3.0, 10.0);
+        float pointSize = 1.0;
+        float slope = tan(1.0 / 2.0);
+        float projFactor =  -0.5 * resolution.y / (slope * mvPosition.z);
+
+        float z = min(0.5 * -gl_Position.z / gl_Position.w, 1.0);
+        gl_PointSize = max(3.0, min(10.0, 0.05 * projFactor));
     }
 
     #include <logdepthbuf_vertex>
